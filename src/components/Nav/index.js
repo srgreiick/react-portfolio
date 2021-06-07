20a483ee5aaa6aac9e63a791f067316c8d4cb688
Anthrax <samgreilick@gmail.com>
2021-06-07 16:44:05 -0600

    style and color

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,26 +1,24 @@
 import React from "react";
-import {Navbar ,Button, FormControl, Nav, Form} from "react-bootstrap"
+import "./style.css";
+import {Navbar , Nav} from "react-bootstrap"
 
 
 
 function NavReturn() {
   return(
-    <div style={{paddingTop:"20px"}}>
+    <div style={{paddingTop:"70px"}}>
 
-    <Navbar fixed="top" bg="light" expand="lg">
+    <Navbar  className={"Nav"} fixed="top"  expand="lg">
   <Navbar.Brand href="/">Samuel Greilick</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link  href="/">Home</Nav.Link>
       <Nav.Link href="/contact">Contact</Nav.Link>
       <Nav.Link href="/portfolio">Portfolio</Nav.Link>
 
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search For Employee" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
+
   </Navbar.Collapse>
 </Navbar>
     </div>
