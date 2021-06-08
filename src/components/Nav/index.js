0074dc5cc7a1e9ac32998db3872291d2983e057b
Anthrax <samgreilick@gmail.com>
2021-06-07 18:29:17 -0600

    red background and style fixes for nav, need to adjust nav button colors next

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -9,13 +9,13 @@ function NavReturn() {
     <div style={{paddingTop:"70px"}}>
 
     <Navbar  className={"Nav"} fixed="top"  expand="lg">
-  <Navbar.Brand href="/">Samuel Greilick</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
+  <Navbar.Brand className={"NavSub"} href="/">Samuel Greilick</Navbar.Brand>
+  <Navbar.Toggle className={"NavSub"} aria-controls="basic-navbar-nav"/>
+  <Navbar.Collapse className={"NavSub"} id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link  href="/">Home</Nav.Link>
-      <Nav.Link href="/contact">Contact</Nav.Link>
-      <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+      <Nav.Link className={"NavSub"} href="/">Home</Nav.Link>
+      <Nav.Link className={"NavSub"} href="/contact">Contact</Nav.Link>
+      <Nav.Link className={"NavSub"} href="/portfolio">Portfolio</Nav.Link>
 
     </Nav>
 
